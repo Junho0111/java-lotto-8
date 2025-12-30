38c30447dd1fe122e477acf857f7c6877f6fe276
Junho0111 <kt27473067@gmail.com>
2025-12-31 07:10:41 +0900

    feat : LottoMachine 추가

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -1,4 +1,22 @@
 package lotto.domain;
 
+import java.util.*;
+
 public class LottoMachine {
+
+    public static List<Integer> getLottoNumber() {
+        List<Integer> lottoNumbers = new ArrayList<>();
+        Random rand = new Random();
+
+        while(lottoNumbers.size() < 6) {
+            int lottoNum = rand.nextInt(45) + 1;
+
+            if(!lottoNumbers.contains(lottoNum)) {
+                lottoNumbers.add(lottoNum);
+            }
+        }
+
+        Collections.sort(lottoNumbers);
+        return lottoNumbers;
+    }
 }
