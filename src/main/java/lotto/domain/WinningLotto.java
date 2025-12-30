86fcbc06389896bc776ae933fea6b9789c2f7cd2
Junho0111 <kt27473067@gmail.com>
2025-12-31 07:20:46 +0900

    feat : WinningLotto 추가

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,4 +1,18 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WinningLotto {
+
+    private List<Integer> winnigLottoList =  new ArrayList<>();
+
+    public WinningLotto(List<Integer> setWinningNumber, int BonusNumber) {
+        setWinningNumber.add(BonusNumber);
+        this.winnigLottoList = setWinningNumber;
+    }
+
+    public List<Integer> getWinnigLottoList() {
+        return winnigLottoList;
+    }
 }
