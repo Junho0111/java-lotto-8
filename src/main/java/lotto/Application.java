ba479ac92d92747b7d3f6b5d483391ae1861afff
Junho0111 <kt27473067@gmail.com>
2025-12-31 08:03:41 +0900

    feat : Application 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,12 @@
 package lotto;
 
+import lotto.contoller.LottoController;
+import lotto.domain.LottoRankEnum;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        LottoController lottoController = new LottoController();
+        lottoController.run();
     }
 }
