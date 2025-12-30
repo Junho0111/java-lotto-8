70c533085ab8ed66707def19ea42eca86e95da69
Junho0111 <kt27473067@gmail.com>
2025-12-31 06:42:13 +0900

    feat : LottoRankEnum 추가

--- src/main/java/lotto/domain/LottoRankEnum.java
+++ src/main/java/lotto/domain/LottoRankEnum.java
@@ -1,4 +1,19 @@
 package lotto.domain;
 
-public class LottoRankEnum {
+public enum LottoRankEnum {
+    RANK_1(2_000_000_000),
+    RANK_2(30_000_000),
+    RANK_3(1_500_000),
+    RANK_4(50_000),
+    RANK_5(5_000);
+
+    private final int amount;
+
+    LottoRankEnum(int amount) {
+        this.amount = amount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
 }
